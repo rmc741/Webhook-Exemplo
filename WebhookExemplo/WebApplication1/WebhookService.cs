313ed3e9383ee5d3d92bbca031c8dd4b39f29fc4
Rafael Mendonca <rafael.medonca2@gmail.com>
2024-07-29 09:16:13 -0300

    exemplo webhook

--- WebhookExemplo/WebApplication1/WebhookService.cs
+++ WebhookExemplo/WebApplication1/WebhookService.cs
@@ -6,5 +6,21 @@
 
     public class WebhookService
     {
+        private readonly List<Subscription> _subscriptions = new();
+        private readonly HttpClient _httpClient = new();
+
+        public void Subscribe(Subscription subscription)
+        {
+            _subscriptions.Add(subscription);
+        }
+
+        public async Task PublishMessage(string topic, object message)
+        {
+            var subscribedWebhooks = _subscriptions.Where(w => w.Topic == topic);
+            foreach (var webhook in subscribedWebhooks)
+            {
+                await _httpClient.PostAsJsonAsync(webhook.Callback, message);
+            }
+        }
     }
 }
